abbdd9f0c431c9e9d9747be1dad354f52b98d33a
Jack Un <jack.un@gmail.com>
2015-03-31 09:49:24 +0300

    Bump to v60

--- 25318.user.js
+++ 25318.user.js
@@ -16,7 +16,7 @@
 // @grant          GM_xmlhttpRequest
 // @grant          GM_registerMenuCommand
 // @grant          unsafeWindow
-// @version        59.7
+// @version        60
 // @updateURL      https://github.com/jackun/VLCTube/raw/master/25318.user.js
 // @downloadURL    https://github.com/jackun/VLCTube/raw/master/25318.user.js
 // ==/UserScript==
